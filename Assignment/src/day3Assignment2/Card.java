9af9af2896d9a8b2c30ff1e8a8dd338b10033e40
Shivaya Gupta <Shivaya.Gupta@Coforge.com>
2026-02-28 21:11:20 +0530

    day4 assignment

---
+++ Assignment/src/day3Assignment2/Card.java
@@ -0,0 +1,34 @@
+package day3Assignment2;
+
+
+
+public class Card extends Payment {
+	
+	long cardNo;
+	int cvv;
+	String expDate;
+	
+	
+	
+	public Card(int amount) {
+		super(amount);
+	}
+	public Card(long cardNo, int cvv, String expDate, int amount) {
+		super(amount);
+		this.cardNo = cardNo;
+		this.cvv = cvv;
+		this.expDate = expDate;
+	}
+	
+	@Override
+	public void pay() {
+		System.out.println("Completed Payment of Rs" + this.amount);
+	}
+	
+	@Override
+	public void paymentReceipt() {
+		System.out.println("Payment Receipt (Card)");
+	    System.out.println("Amount Paid: " + this.amount + "Rs.");
+	}
+}
+
---
+++ Assignment/src/day3Assignment2/Payment.java
@@ -0,0 +1,19 @@
+package day3Assignment2;
+
+public class Payment {
+	int amount;
+	public Payment(int amount) {
+		this.amount = amount;
+	}
+	public Payment() {
+		super();
+	}
+	
+	public void pay() {
+		
+	}
+	
+	public void paymentReceipt() {
+		
+	}
+}
