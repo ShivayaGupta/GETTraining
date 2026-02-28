26c271c75ddbdf53c170a29474da9759b5988ed7
Shivaya Gupta <Shivaya.Gupta@Coforge.com>
2026-02-28 21:23:26 +0530

    day3 assignment

---
+++ Assignment/src/day4Assignment/deletion.java
@@ -0,0 +1,5 @@
+package day4Assignment;
+
+public class deletion {
+
+}
---
+++ Assignment/src/day4Assignment/insertionAtLast.java
@@ -0,0 +1,10 @@
+package day4Assignment;
+
+public class insertionAtLast {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
